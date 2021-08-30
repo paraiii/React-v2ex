e440bd789e7f6baf9200458d73fb30953fe34b24
paraiiii <84451274+paraiiii@users.noreply.github.com>
2021-08-30 18:53:06 +1000

    fix folder.

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -3,18 +3,16 @@ import './Home.css';
 import Content from '../Content/Content';
 import Header from '../Header/Header';
 import HotTopics from '../HotTopics/HotTopics';
-import { StyleSheet, View } from "react-native";
 
 
 function Home() {
     return ( 
         <div>
             <Header />           
-                <div className="main">
-                              <Content />
-                              <HotTopics />
-                   </div>
-           
+            <div className="main">
+                <Content />
+                <HotTopics />
+            </div>
         </div>
     )
 };
