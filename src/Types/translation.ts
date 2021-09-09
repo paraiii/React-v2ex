3e0452b931749c8a411482eb40000f21126a26d9
paraiiii <84451274+paraiiii@users.noreply.github.com>
2021-09-09 17:32:20 +1000

    update

--- src/Types/translation.ts
+++ src/Types/translation.ts
@@ -1,13 +1,9 @@
 export interface TranslationItem {
-    // key: string,
-    // value:string,
-    // default: string,
     reply_time: string,
     last_reply_from: string,
 }
 
 export interface Translation {
-    // data: Map<string, TranslationItem>
     cn: TranslationItem []
     en: TranslationItem []
 }
