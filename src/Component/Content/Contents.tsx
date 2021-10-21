2c7ca4ae165056e4693e07dff731e3ea653f3d9e
paraiiii <84451274+paraiiii@users.noreply.github.com>
2021-10-21 17:48:15 +1100

    update-for-netlify2

--- src/Component/Content/Contents.tsx
+++ src/Component/Content/Contents.tsx
@@ -39,7 +39,7 @@ export const Contents = () => {
                 })
             }
             
-        }, []);
+        }, [id]);
     
     //加callback
     const mapContentDataToContentLine = useCallback((contentData : any) => {
